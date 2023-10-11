67c8d8790bff8e9a683cfa475ee770fc884864ae
JovanWang <1378744968@qq.com>
2023-10-11 17:40:57 +0800

    Update config.py
    update personal profile

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # The repository to add this issue to
-REPO_OWNER = 'DongZhouGu'
+REPO_OWNER = 'JovanWang'
 REPO_NAME = 'arxiv-daily'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["human object interaction", "visual relation detection", "object detection", "transformer",
-                "scene understanding", "visual reasoning"]
+KEYWORD_LIST = ["Tensor Core", "SIMD", "TCU", "Sparse Martix", "Sparse Tensor", "Single Instruction stream Single Data"]
