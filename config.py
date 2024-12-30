1736c6f2e9cc9c80c11b3bfdcc63df44fa974fc7
JovanWang <1378744968@qq.com>
2024-12-30 15:09:30 +0800

    Update config.py
    add sparse keyword

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ REPO_NAME = 'arxiv-daily'
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["Sparse Martix", "Sparse Tensor", "Sparse Compiler", "Code Generation", "Generative Model", "Mental Model", "LLM-Based Agent", "C language code", "Cognitive Architecture"]
+KEYWORD_LIST = ["Sparse", "Sparse Martix", "Sparse Tensor", "Sparse Compiler", "Code Generation", "Generative Model", "Mental Model", "LLM-Based Agent", "C language code", "Cognitive Architecture"]
